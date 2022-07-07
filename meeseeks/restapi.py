c76d6e0f69b06866c7240d88e0a590d3d30ba0bc
Artem Lavrukhin <lavryha4590@gmail.com>
2022-07-07 16:28:23 +0300

    Rename methods and docstrings

--- meeseeks/restapi.py
+++ meeseeks/restapi.py
@@ -99,8 +99,8 @@ class RestAPI:
 
         return await self.make_request(settings.CHAT_REACT_POST_REQUEST, 'post', msg)
 
-    async def create_private_room(self, name: str, users: list) -> dict[str, Any]:
-        """Create private room. """
+    async def create_group(self, name: str, users: list) -> dict[str, Any]:
+        """Create group. """
 
         msg: str = json.dumps({
             'name': name,
@@ -109,8 +109,8 @@ class RestAPI:
 
         return await self.make_request(settings.GROUPS_CREATE_POST_REQUEST, 'post', msg)
 
-    async def delete_private_room(self, name: str) -> dict[str, Any]:
-        """Delete private room. """
+    async def delete_group(self, name: str) -> dict[str, Any]:
+        """Delete group. """
 
         msg: str = json.dumps({
             'roomName': name,
